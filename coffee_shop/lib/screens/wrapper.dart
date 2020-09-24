be9962c5dd9ab135cb47da72341122a3896f3d73
emineinan <emineinan@users.noreply.github.com>
2020-09-24 10:13:21 +0300

    anonymous sign in is added.

--- coffee_shop/lib/screens/wrapper.dart
+++ coffee_shop/lib/screens/wrapper.dart
@@ -1,3 +1,4 @@
+import 'package:coffee_shop/screens/authenticate/authenticate.dart';
 import 'package:coffee_shop/screens/home/home.dart';
 import 'package:flutter/material.dart';
 
@@ -7,6 +8,6 @@ class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //return either Home or Authenticate widget
-    return Home();
+    return Authenticate();
   }
 }
