99ef76c556a74a9bdbc82498a14dc130427e7404
emineinan <emineinan@users.noreply.github.com>
2020-09-24 16:39:36 +0300

    provider package is added.

--- coffee_shop/lib/main.dart
+++ coffee_shop/lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:coffee_shop/models/user.dart';
 import 'package:coffee_shop/screens/wrapper.dart';
+import 'package:coffee_shop/services/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,8 +12,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Wrapper(),
+    return StreamProvider<User>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+        home: Wrapper(),
+      ),
     );
   }
 }
--- coffee_shop/lib/screens/wrapper.dart
+++ coffee_shop/lib/screens/wrapper.dart
@@ -1,12 +1,16 @@
+import 'package:coffee_shop/models/user.dart';
 import 'package:coffee_shop/screens/authenticate/authenticate.dart';
 import 'package:coffee_shop/screens/home/home.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   const Wrapper({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<User>(context);
+    print(user);
     //return either Home or Authenticate widget
     return Authenticate();
   }
