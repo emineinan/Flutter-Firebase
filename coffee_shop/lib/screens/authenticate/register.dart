97ecff49381bd935ae5843aaa67ce9a5923e0c2b
emineinan <emineinan@users.noreply.github.com>
2020-09-25 11:55:22 +0300

    registration with email and password is added.

--- coffee_shop/lib/screens/authenticate/register.dart
+++ coffee_shop/lib/screens/authenticate/register.dart
@@ -11,10 +11,12 @@ class Register extends StatefulWidget {
 
 class _RegisterState extends State<Register> {
   final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
 
   //text field state
   String email = "";
   String password = "";
+  String error = "";
 
   @override
   Widget build(BuildContext context) {
@@ -36,12 +38,16 @@ class _RegisterState extends State<Register> {
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
         child: Form(
+          key: _formKey,
           child: Column(
             children: <Widget>[
               SizedBox(
                 height: 20.0,
               ),
               TextFormField(
+                validator: (value) {
+                  return value.isEmpty ? "Enter an email!" : null;
+                },
                 onChanged: (value) {
                   setState(() {
                     email = value;
@@ -52,6 +58,11 @@ class _RegisterState extends State<Register> {
                 height: 20.0,
               ),
               TextFormField(
+                validator: (value) {
+                  return value.length < 6
+                      ? "Enter an password 6+ chars long!"
+                      : null;
+                },
                 obscureText: true,
                 onChanged: (value) {
                   setState(() {
@@ -64,14 +75,26 @@ class _RegisterState extends State<Register> {
               ),
               RaisedButton(
                 onPressed: () async {
-                  print(email);
-                  print(password);
+                  if (_formKey.currentState.validate()) {
+                    dynamic result = await _auth.registerWithEmailAndPassword(
+                        email, password);
+                    if (result == null) {
+                      setState(() => error = "please supply a valid email!");
+                    }
+                  }
                 },
                 color: Colors.deepOrangeAccent,
                 child: Text(
                   "Register",
                   style: TextStyle(color: Colors.white),
                 ),
+              ),
+              SizedBox(
+                height: 12.0,
+              ),
+              Text(
+                error,
+                style: TextStyle(color: Colors.white, fontSize: 20.0),
               )
             ],
           ),
