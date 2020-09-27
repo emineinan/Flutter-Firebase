3c83d8bdd23a43650471d7d7c8248b877fbbc349
emineinan <emineinan@users.noreply.github.com>
2020-09-27 15:48:45 +0300

    user data model is added.

--- coffee_shop/lib/services/database.dart
+++ coffee_shop/lib/services/database.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:coffee_shop/models/brew.dart';
+import 'package:coffee_shop/models/user.dart';
 
 class DatabaseService {
   final String uid;
@@ -27,8 +28,23 @@ class DatabaseService {
     }).toList();
   }
 
+  //userData from snapshot
+  UserData _userDataFromSnapshot(DocumentSnapshot snapshot) {
+    return UserData(
+      uid: uid,
+      name: snapshot.data["name"],
+      sugars: snapshot.data["sugars"],
+      strength: snapshot.data["strength"],
+    );
+  }
+
   //get bres stream
   Stream<List<Brew>> get brews {
     return brewClollection.snapshots().map(_brewListFromSnapshot);
+  }
+
+  //get user doc stream
+  Stream<UserData> get userData {
+    return brewClollection.document(uid).snapshots().map(_userDataFromSnapshot);
   }
 }
