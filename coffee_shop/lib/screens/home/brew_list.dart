fcf87748a667daae5281fcf40e91b591ce9bb063
emineinan <emineinan@users.noreply.github.com>
2020-09-28 10:59:43 +0300

    user data is updated.

--- coffee_shop/lib/screens/home/brew_list.dart
+++ coffee_shop/lib/screens/home/brew_list.dart
@@ -13,7 +13,7 @@ class BrewList extends StatefulWidget {
 class _BrewListState extends State<BrewList> {
   @override
   Widget build(BuildContext context) {
-    final brews = Provider.of<List<Brew>>(context);
+    final brews = Provider.of<List<Brew>>(context) ?? [];
     return ListView.builder(
         itemCount: brews.length,
         itemBuilder: (context, index) {
--- coffee_shop/lib/screens/home/settings_form.dart
+++ coffee_shop/lib/screens/home/settings_form.dart
@@ -78,9 +78,14 @@ class _SettingFormState extends State<SettingForm> {
                         style: TextStyle(color: Colors.white),
                       ),
                       onPressed: () async {
-                        print(_currentName);
-                        print(_currentSugars);
-                        print(_currentStrength);
+                        if (_formKey.currentState.validate()) {
+                          await DatabaseService(uid: user.uid).updateUserData(
+                            _currentSugars ?? userData.sugars,
+                            _currentName ?? userData.name,
+                            _currentStrength ?? userData.strength,
+                          );
+                          Navigator.pop(context);
+                        }
                       }),
                 ],
               ),
