2fb49c06015b26afeebefbef78c426d0c757d494
emineinan <emineinan@users.noreply.github.com>
2020-10-07 17:37:55 +0300

    listing characters is added.

--- breaking_bad_app/lib/screens/character_list.dart
+++ breaking_bad_app/lib/screens/character_list.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:breaking_bad_app/model/character.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 
@@ -11,26 +12,48 @@ class CharacterList extends StatefulWidget {
 }
 
 class _CharacterListState extends State<CharacterList> {
+  List<Character> characterList = new List<Character>();
+
   void getCharacters() async {
     Response result = await get("https://breakingbadapi.com/api/characters");
     var data = await jsonDecode(result.body);
-    print(data[0]["name"]);
+    setState(() {
+      for (var i = 0; i < data.length; i++) {
+        Character c = Character();
+        c.id = data[i]["id"];
+        c.name = data[i]["name"];
+        c.img = data[i]["img"];
+        c.nickname = data[i]["nickname"];
+
+        characterList.add(c);
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    getCharacters();
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Characters"),
-      ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            getCharacters();
-          },
-          child: Text("Get Characters"),
+        appBar: AppBar(
+          title: Text("Characters"),
         ),
-      ),
-    );
+        body: Container(
+          child: ListView.builder(
+              itemCount: characterList.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(characterList[index].name),
+                  subtitle: Text(characterList[index].nickname),
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(characterList[index].img),
+                  ),
+                );
+              }),
+        ));
   }
 }
