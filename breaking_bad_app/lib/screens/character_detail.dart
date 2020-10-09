470db4e9eeb2de3dcd98caf3f810e8ecfa533c19
emineinan <emineinan@users.noreply.github.com>
2020-10-09 16:57:18 +0300

    spinkit is added.

--- breaking_bad_app/lib/screens/character_detail.dart
+++ breaking_bad_app/lib/screens/character_detail.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
+import 'package:breaking_bad_app/model/character.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:http/http.dart';
 
 class CharacterDetail extends StatefulWidget {
   final int id;
@@ -9,9 +14,18 @@ class CharacterDetail extends StatefulWidget {
 }
 
 class _CharacterDetailState extends State<CharacterDetail> {
+  Character character = new Character();
+  bool isLoad = false;
+
   void getCharacterwithId() async {
-    print("id: ");
-    print(widget.id);
+    Response response =
+        await get("https://breakingbadapi.com/api/characters/${widget.id}");
+    var data = await jsonDecode(response.body); //map
+
+    setState(() {
+      character.name = data[0]["name"];
+      isLoad = true;
+    });
   }
 
   @override
@@ -22,6 +36,19 @@ class _CharacterDetailState extends State<CharacterDetail> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return isLoad
+        ? Scaffold(
+            appBar: AppBar(
+              title: Text(character.name),
+            ),
+          )
+        : Scaffold(
+            body: Center(
+              child: SpinKitThreeBounce(
+                color: Colors.redAccent,
+                size: 70.0,
+              ),
+            ),
+          );
   }
 }
