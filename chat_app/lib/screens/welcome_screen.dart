c57e2188a4cc5affd236458ddced94cbffa3c72e
emineinan <emineinan@users.noreply.github.com>
2020-10-21 10:48:35 +0300

    welcome screen is added.

--- chat_app/lib/screens/welcome_screen.dart
+++ chat_app/lib/screens/welcome_screen.dart
@@ -10,8 +10,59 @@ class WelcomeScreen extends StatefulWidget {
 class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Welcome"),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 100.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                SizedBox(
+                  width: 50.0,
+                ),
+                Container(
+                  child: Image.asset("images/logo.png"),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 40.0,
+            ),
+            Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+                child: Material(
+                  elevation: 5.0,
+                  color: Colors.pink[100],
+                  borderRadius: BorderRadius.circular(30),
+                  child: MaterialButton(
+                    onPressed: () {},
+                    minWidth: 170.0,
+                    height: 30.0,
+                    child: Text("Login"),
+                  ),
+                )),
+            SizedBox(
+              height: 10.0,
+            ),
+            Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+                child: Material(
+                  elevation: 5.0,
+                  color: Colors.pink[100],
+                  borderRadius: BorderRadius.circular(30),
+                  child: MaterialButton(
+                    onPressed: () {},
+                    minWidth: 170.0,
+                    height: 40.0,
+                    child: Text("Register"),
+                  ),
+                ))
+          ],
+        ),
+      ),
     );
   }
 }
