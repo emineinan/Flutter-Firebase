31b84e17296c5457d44e277fa5a6c89f7c1acb76
emineinan <emineinan@users.noreply.github.com>
2020-10-21 10:12:44 +0300

    project init.

---
+++ chat_app/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:chat_app/screens/chat_screen.dart';
+import 'package:chat_app/screens/login_screen.dart';
+import 'package:chat_app/screens/register_screen.dart';
+import 'package:chat_app/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      initialRoute: "welcomeScreen",
+      routes: {
+        "welcomeScreen": (context) => WelcomeScreen(),
+        "registerScreen": (context) => RegisterScreen(),
+        "loginScreen": (context) => LoginScreen(),
+        "chatScreen": (context) => ChatScreen()
+      },
+    );
+  }
+}
---
+++ chat_app/lib/screens/register_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatefulWidget {
+  RegisterScreen({Key key}) : super(key: key);
+
+  @override
+  _RegisterScreenState createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Text("Register"));
+  }
+}
