7b9afb9ef1264b0e471ab6c23d2f5c5670a82bab
emineinan <emineinan@users.noreply.github.com>
2020-10-24 11:24:38 +0300

    spinner is added.

--- chat_app/lib/screens/login_screen.dart
+++ chat_app/lib/screens/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:chat_app/screens/chat_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class LoginScreen extends StatefulWidget {
   static const String id = "loginScreen";
@@ -14,6 +15,7 @@ class _LoginScreenState extends State<LoginScreen>
   final auth = FirebaseAuth.instance;
   String email;
   String password;
+  bool isLoad = false;
 
   AnimationController controller;
 
@@ -33,98 +35,107 @@ class _LoginScreenState extends State<LoginScreen>
     return Scaffold(
       resizeToAvoidBottomPadding: false,
       backgroundColor: Colors.white.withOpacity(controller.value),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 100.0),
-        child: Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Hero(
-                tag: "logo",
-                child: Container(
-                  child: Image.asset("images/logo2.png"),
+      body: ModalProgressHUD(
+        inAsyncCall: isLoad,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 100.0),
+          child: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Hero(
+                  tag: "logo",
+                  child: Container(
+                    child: Image.asset("images/logo2.png"),
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              TextField(
-                keyboardType: TextInputType.emailAddress,
-                onChanged: (value) {
-                  email = value;
-                },
-                decoration: InputDecoration(
-                    hintText: "Email",
-                    hintStyle: TextStyle(color: Color(0xFF574b90)),
-                    contentPadding:
-                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(32)),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        width: 3,
-                        color: Color(0xFF574b90),
+                SizedBox(
+                  height: 30,
+                ),
+                TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  onChanged: (value) {
+                    email = value;
+                  },
+                  decoration: InputDecoration(
+                      hintText: "Email",
+                      hintStyle: TextStyle(color: Color(0xFF574b90)),
+                      contentPadding: EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 15.0),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(32)),
                       ),
-                      borderRadius: BorderRadius.all(Radius.circular(32)),
-                    )),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              TextField(
-                keyboardType: TextInputType.visiblePassword,
-                obscureText: true,
-                onChanged: (value) {
-                  password = value;
-                },
-                decoration: InputDecoration(
-                    hintText: "Password",
-                    hintStyle: TextStyle(color: Color(0xFF574b90)),
-                    contentPadding:
-                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(32)),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        width: 3,
-                        color: Color(0xFF574b90),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 3,
+                          color: Color(0xFF574b90),
+                        ),
+                        borderRadius: BorderRadius.all(Radius.circular(32)),
+                      )),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                TextField(
+                  keyboardType: TextInputType.visiblePassword,
+                  obscureText: true,
+                  onChanged: (value) {
+                    password = value;
+                  },
+                  decoration: InputDecoration(
+                      hintText: "Password",
+                      hintStyle: TextStyle(color: Color(0xFF574b90)),
+                      contentPadding: EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 15.0),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(32)),
                       ),
-                      borderRadius: BorderRadius.all(Radius.circular(32)),
-                    )),
-              ),
-              SizedBox(
-                height: 30.0,
-              ),
-              Padding(
-                  padding:
-                      EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
-                  child: Material(
-                    elevation: 5.0,
-                    color: Color(0xFF574b90),
-                    borderRadius: BorderRadius.circular(30),
-                    child: MaterialButton(
-                      onPressed: () async {
-                        try {
-                          final user = await auth.signInWithEmailAndPassword(
-                              email: email, password: password);
-                          if (user != null) {
-                            Navigator.pushNamed(context, ChatScreen.id);
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 3,
+                          color: Color(0xFF574b90),
+                        ),
+                        borderRadius: BorderRadius.all(Radius.circular(32)),
+                      )),
+                ),
+                SizedBox(
+                  height: 30.0,
+                ),
+                Padding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+                    child: Material(
+                      elevation: 5.0,
+                      color: Color(0xFF574b90),
+                      borderRadius: BorderRadius.circular(30),
+                      child: MaterialButton(
+                        onPressed: () async {
+                          try {
+                            setState(() {
+                              isLoad = true;
+                            });
+                            final user = await auth.signInWithEmailAndPassword(
+                                email: email, password: password);
+                            if (user != null) {
+                              setState(() {
+                                isLoad = true;
+                              });
+                              Navigator.pushNamed(context, ChatScreen.id);
+                            }
+                          } catch (e) {
+                            print(e.message);
                           }
-                        } catch (e) {
-                          print(e.message);
-                        }
-                      },
-                      minWidth: 170.0,
-                      child: Text(
-                        "Login",
-                        style: TextStyle(color: Colors.white),
+                        },
+                        minWidth: 170.0,
+                        child: Text(
+                          "Login",
+                          style: TextStyle(color: Colors.white),
+                        ),
                       ),
-                    ),
-                  ))
-            ],
+                    ))
+              ],
+            ),
           ),
         ),
       ),
--- chat_app/lib/screens/register_screen.dart
+++ chat_app/lib/screens/register_screen.dart
@@ -1,6 +1,7 @@
 import 'package:chat_app/screens/chat_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class RegisterScreen extends StatefulWidget {
   static const String id = "registerScreen";
@@ -14,6 +15,7 @@ class _RegisterScreenState extends State<RegisterScreen>
   final auth = FirebaseAuth.instance;
   String email;
   String password;
+  bool isLoad = false;
 
   AnimationController controller;
   Animation animationBorder;
@@ -39,99 +41,108 @@ class _RegisterScreenState extends State<RegisterScreen>
     return Scaffold(
       resizeToAvoidBottomPadding: false,
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 100.0),
-        child: Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Hero(
-                tag: "logo",
-                child: Container(
-                  child: Image.asset("images/logo2.png"),
+      body: ModalProgressHUD(
+        inAsyncCall: isLoad,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 100.0),
+          child: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Hero(
+                  tag: "logo",
+                  child: Container(
+                    child: Image.asset("images/logo2.png"),
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              TextField(
-                keyboardType: TextInputType.emailAddress,
-                onChanged: (value) {
-                  email = value;
-                },
-                decoration: InputDecoration(
-                    hintText: "Email",
-                    hintStyle: TextStyle(color: Color(0xFF574b90)),
-                    contentPadding:
-                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
-                    border: OutlineInputBorder(
-                      borderRadius: animationBorder.value,
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        width: 3,
-                        color: Color(0xFF574b90),
+                SizedBox(
+                  height: 30,
+                ),
+                TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  onChanged: (value) {
+                    email = value;
+                  },
+                  decoration: InputDecoration(
+                      hintText: "Email",
+                      hintStyle: TextStyle(color: Color(0xFF574b90)),
+                      contentPadding: EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 15.0),
+                      border: OutlineInputBorder(
+                        borderRadius: animationBorder.value,
                       ),
-                      borderRadius: animationBorder.value,
-                    )),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              TextField(
-                keyboardType: TextInputType.visiblePassword,
-                obscureText: true,
-                onChanged: (value) {
-                  password = value;
-                },
-                decoration: InputDecoration(
-                    hintText: "Password",
-                    hintStyle: TextStyle(color: Color(0xFF574b90)),
-                    contentPadding:
-                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
-                    border: OutlineInputBorder(
-                      borderRadius: animationBorder.value,
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        width: 3,
-                        color: Color(0xFF574b90),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 3,
+                          color: Color(0xFF574b90),
+                        ),
+                        borderRadius: animationBorder.value,
+                      )),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                TextField(
+                  keyboardType: TextInputType.visiblePassword,
+                  obscureText: true,
+                  onChanged: (value) {
+                    password = value;
+                  },
+                  decoration: InputDecoration(
+                      hintText: "Password",
+                      hintStyle: TextStyle(color: Color(0xFF574b90)),
+                      contentPadding: EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 15.0),
+                      border: OutlineInputBorder(
+                        borderRadius: animationBorder.value,
                       ),
-                      borderRadius: animationBorder.value,
-                    )),
-              ),
-              SizedBox(
-                height: 30.0,
-              ),
-              Padding(
-                  padding:
-                      EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
-                  child: Material(
-                    elevation: 5.0,
-                    color: Color(0xFF574b90),
-                    borderRadius: BorderRadius.circular(30),
-                    child: MaterialButton(
-                      onPressed: () async {
-                        try {
-                          final newUser =
-                              await auth.createUserWithEmailAndPassword(
-                                  email: email, password: password);
-                          if (newUser != null) {
-                            Navigator.pushNamed(context, ChatScreen.id);
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 3,
+                          color: Color(0xFF574b90),
+                        ),
+                        borderRadius: animationBorder.value,
+                      )),
+                ),
+                SizedBox(
+                  height: 30.0,
+                ),
+                Padding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+                    child: Material(
+                      elevation: 5.0,
+                      color: Color(0xFF574b90),
+                      borderRadius: BorderRadius.circular(30),
+                      child: MaterialButton(
+                        onPressed: () async {
+                          setState(() {
+                            isLoad = true;
+                          });
+                          try {
+                            final newUser =
+                                await auth.createUserWithEmailAndPassword(
+                                    email: email, password: password);
+                            if (newUser != null) {
+                              setState(() {
+                                isLoad = false;
+                              });
+                              Navigator.pushNamed(context, ChatScreen.id);
+                            }
+                          } catch (e) {
+                            print(e.message);
                           }
-                        } catch (e) {
-                          print(e.message);
-                        }
-                      },
-                      minWidth: 170.0,
-                      child: Text(
-                        "Register",
-                        style: TextStyle(color: Colors.white),
+                        },
+                        minWidth: 170.0,
+                        child: Text(
+                          "Register",
+                          style: TextStyle(color: Colors.white),
+                        ),
                       ),
-                    ),
-                  ))
-            ],
+                    ))
+              ],
+            ),
           ),
         ),
       ),
