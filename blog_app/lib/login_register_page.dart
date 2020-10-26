b8fd9535b850b6683492bc3e8fbba244c0dc9242
emineinan <emineinan@users.noreply.github.com>
2020-10-26 17:38:55 +0300

    project init.

---
+++ blog_app/lib/login_register_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class LoginRegisterPage extends StatefulWidget {
+  LoginRegisterPage({Key key}) : super(key: key);
+
+  @override
+  _LoginRegisterPageState createState() => _LoginRegisterPageState();
+}
+
+class _LoginRegisterPageState extends State<LoginRegisterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomPadding: true,
+      body: Container(),
+      backgroundColor: Color(0xFFf6e58d),
+    );
+  }
+}
