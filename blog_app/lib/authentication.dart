dda2640757ccc4e49ed645c7800c7d7c20e60dd1
emineinan <emineinan@users.noreply.github.com>
2020-10-28 15:26:58 +0300

    firebase auth methods added.

---
+++ blog_app/lib/authentication.dart
@@ -0,0 +1,37 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+abstract class AuthOperation {
+  Future<String> signIn(String email, String password);
+  Future<String> signUp(String email, String password);
+  Future<void> signOut();
+  Future<String> getCurrentUser();
+}
+
+class AuthService implements AuthOperation {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<String> signIn(String email, String password) async {
+    AuthResult result = await _auth.signInWithEmailAndPassword(
+        email: email, password: password);
+
+    FirebaseUser user = result.user;
+    return user.uid;
+  }
+
+  Future<String> signUp(String email, String password) async {
+    AuthResult result = await _auth.createUserWithEmailAndPassword(
+        email: email, password: password);
+
+    FirebaseUser user = result.user;
+    return user.uid;
+  }
+
+  Future<void> signOut() async {
+    await _auth.signOut();
+  }
+
+  Future<String> getCurrentUser() async {
+    FirebaseUser user = await _auth.currentUser();
+    return user.uid;
+  }
+}
